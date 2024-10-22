7e40ca9bdcd9ddadf0842d1aa60a24f9331a568e
sofi16mar <157255753+sofi16mar@users.noreply.github.com>
2024-10-22 09:55:58 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,14 +1,11 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-#define maxproduc 10
+void ingresarProducto(char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
+void mostrarProductos(char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
+float calcTotal(float precios[10], int cantidades[10], int cantidad);
+float calcPromedio(float precios[10], int cantidades[10], int cantidad);
+void buscarProducto(char nombreBuscado[50], char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
 
-// Declaración de funciones
-void ingresarProducto(char nombres[10][50], float precios[10], int cantidad);
-void mostrarProductos(char nombres[10][50], float precios[10], int cantidad);
-float calcTotal(float precios[10], int cantidad);
-float calcPromedio(float precios[10], int cantidad);
-void buscarProducto(char nombreBuscado[10], char nombres[10][50], float precios[10], int cantidad);
-void encontrarMaxMin(float precios[10], char nombres[10][50], int cantidad);
+#endif // FUNCIONES_H
 
-#endif
