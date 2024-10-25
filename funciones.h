ba9ef7a219491a04bbaece05bc5eb14ab5da98a3
sofi16mar <157255753+sofi16mar@users.noreply.github.com>
2024-10-24 21:47:46 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,11 +1,11 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+// Prototipos de las funciones
 void ingresarProducto(char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
 void mostrarProductos(char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
 float calcTotal(float precios[10], int cantidades[10], int cantidad);
 float calcPromedio(float precios[10], int cantidades[10], int cantidad);
 void buscarProducto(char nombreBuscado[50], char nombres[10][50], float precios[10], int cantidades[10], int cantidad);
 
-#endif // FUNCIONES_H
-
+#endif
